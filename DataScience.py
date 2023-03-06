433458076a119c398dd99afbac6b83db6fb3a9a3
Suresh Dokka <112100121+Suresh-d-7@users.noreply.github.com>
2023-03-06 20:47:09 +0530

    Update DataScience.py

--- DataScience.py
+++ DataScience.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[44]:
-
-
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
@@ -16,10 +10,6 @@ results = model.fit()
 prediction = results.forecast()[0]
 print("The predicted price for the next day is:", prediction)
 
-
-# In[47]:
-
-
 def predict_next_day_price():
     data = yf.download("^NSEI", period="1y")
     y = data['Adj Close']
@@ -28,15 +18,9 @@ def predict_next_day_price():
     prediction = results.forecast()[0]
     return prediction
 
-
-# In[46]:
-
-
 next_day_price = predict_next_day_price()
 print("The predicted price for the next day is:", next_day_price)
 
-
-# In[ ]:
 
 
 
